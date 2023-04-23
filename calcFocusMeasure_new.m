a0a66e501b22a6910ab62b7d8e3a8a50f61e782d
ixilai <118871138+ixilai@users.noreply.github.com>
2023-04-23 15:18:54 +0800

    Add files via upload

---
+++ calcFocusMeasure_new.m
@@ -0,0 +1,60 @@
+function out = calcFocusMeasure_new(img, bb, fmeasure)
+
+switch fmeasure
+                  
+    case 'SAG'
+        H = fspecial('sobel');
+        img = mirror_extend(img,1,1);
+        Gx = conv2(img,H,'valid');
+        Gy = conv2(img,H','valid');
+        grad = abs(Gx) + abs(Gy);
+        out = imfilter(grad,fspecial('average', bb),'symmetric');        
+        
+    case 'VOI'
+        [M, N] = size(img);
+        I_ext = mirror_extend(img,(bb-1)/2,(bb-1)/2);
+        fun = @(x) std(x);
+        tmp = colfilt(I_ext,[bb bb],'sliding',fun);
+        out = tmp((bb+1)/2:M+(bb-1)/2,(bb+1)/2:N+(bb-1)/2);                    
+        
+    case 'SF'
+        [M, N] = size(img);
+        I_ext = mirror_extend(img,1,1);
+        h=[0 1 -1];
+        diffh = conv2c(I_ext,h);
+        diffh = diffh(2:M+1,2:N+1);
+        h=[0 1 -1]';
+        diffv = conv2c(I_ext,h);
+        diffv = diffv(2:M+1,2:N+1);
+        RF2 = imfilter(diffv.^2,fspecial('average', bb),'symmetric');
+        CF2 = imfilter(diffh.^2,fspecial('average', bb),'symmetric');
+        out = sqrt(RF2+CF2);
+        
+   case 'EOL'
+        H = fspecial('laplacian');
+        img = mirror_extend(img,1,1);
+        L = conv2(img,H,'valid');
+        if (bb>0)
+            out = imfilter(L.^2,fspecial('gaussian', bb,bb/6),'symmetric');
+        else
+            out = L.^2;
+        end
+        
+   case 'AOL'
+        H = fspecial('laplacian');
+        img = mirror_extend(img,1,1);
+        L = conv2(img,H,'valid');
+        if (bb>0)
+            out = imfilter(abs(L),fspecial('gaussian', bb,bb/6),'symmetric');
+        else
+            out = abs(L);
+        end
+    case 'LPC'
+        [LPC_SI lpc_map] = lpc_si(img);
+        LPC_SI
+        out=lpc_map;
+    case 'STO'
+        out=str_tensor_map(img,1);
+end
+return
+
